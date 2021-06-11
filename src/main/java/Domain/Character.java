80d68446b362e98dd26857e7a72a2b16bd9a61df
Kevin <kdoan@qa.com>
2021-06-11 10:07:39 +0100

    KD: Created domain classes

---
+++ src/main/java/Domain/Character.java
@@ -0,0 +1,126 @@
+package Domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Character {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Auto Increment
+	private Integer id;
+	
+	
+	private String name;
+	private String weapon;
+	private String element;
+	private Long level;
+	
+	@ManyToOne
+	private Team team;
+	
+	public Character(Integer id, String name, String weapon, String element, Long level) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.weapon = weapon;
+		this.element = element;
+		this.level = level;
+	}
+	public Character(String name, String weapon, String element, Long level) {
+		super();
+		this.name = name;
+		this.weapon = weapon;
+		this.element = element;
+		this.level = level;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getWeapon() {
+		return weapon;
+	}
+	public void setWeapon(String weapon) {
+		this.weapon = weapon;
+	}
+	public String getElement() {
+		return element;
+	}
+	public void setElement(String element) {
+		this.element = element;
+	}
+	public Long getLevel() {
+		return level;
+	}
+	public void setLevel(Long level) {
+		this.level = level;
+	}
+	
+	@Override
+	public String toString() {
+		return "Character [id=" + id + ", name=" + name + ", weapon=" + weapon + ", element=" + element + ", level="
+				+ level + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((element == null) ? 0 : element.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((level == null) ? 0 : level.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((weapon == null) ? 0 : weapon.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Character other = (Character) obj;
+		if (element == null) {
+			if (other.element != null)
+				return false;
+		} else if (!element.equals(other.element))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (level == null) {
+			if (other.level != null)
+				return false;
+		} else if (!level.equals(other.level))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (weapon == null) {
+			if (other.weapon != null)
+				return false;
+		} else if (!weapon.equals(other.weapon))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ src/main/java/Domain/Team.java
@@ -0,0 +1,51 @@
+package Domain;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Team {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	
+	@OneToMany(mappedBy = "garage")
+	@JsonIgnore
+	private List <Character> characters;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Character> getCharacters() {
+		return characters;
+	}
+
+	public void setCharacters(List<Character> characters) {
+		this.characters = characters;
+	}
+	
+	
+}
